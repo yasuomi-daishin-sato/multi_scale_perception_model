40e0745a39463f4b91f338509f334179a8e6f9c9
yasuomi-daishin-sato <35786859+yasuomi-daishin-sato@users.noreply.github.com>
2018-01-26 11:17:51 +0900

    Update stdafx.h

--- stdafx.h
+++ stdafx.h
@@ -14,3 +14,19 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/gpu/gpu.hpp"
 
+#ifdef _DEBUG
+//モードの場合Debug
+#pragma comment( lib, "opencv_core249d.lib" )
+#pragma comment( lib, "opencv_imgproc249d.lib" )
+#pragma comment( lib, "opencv_highgui249d.lib" )
+#pragma comment( lib, "opencv_video249d.lib" )
+#else
+//モードの場合Release
+#pragma comment( lib, "opencv_core249.lib" )
+#pragma comment( lib, "opencv_imgproc249.lib" )
+#pragma comment( lib, "opencv_highgui249.lib" )
+#pragma comment( lib, "opencv_video249.lib" )
+#endif
+
+using namespace std;
+using namespace cv;
